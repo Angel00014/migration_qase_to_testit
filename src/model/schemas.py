e6447cd97392550161a331cd7e363674aa1824ab
Alex_work_note <alexchugunov4@gmail.com>
2024-10-05 13:06:50 +0300

    refactor schemas.py(baseModel in last)

--- src/model/schemas.py
+++ src/model/schemas.py
@@ -1,40 +1,34 @@
-from typing import List, Dict, Optional
-
-from pydantic import BaseModel
-
-
-class ItemTestCase(BaseModel):
-    test_case: str
+from pydantic import BaseModel, Field
 
 
 class ReportDataV2(BaseModel):
-    qase_url: str = None
-    qase_number_test_cases: List[int]
+    qase_url: str | None
+    qase_number_test_cases: list[int]
     qase_token: str
     qase_project_name: str
     testit_token: str
-    testit_cookies: str = None
+    testit_cookies: str | None
     testit_projectId: str
     testit_sectionId: str
     testit_url: str
 
 
 class ReportData(BaseModel):
-    number_test_cases: List[int]
+    number_test_cases: list[int]
     qase_cookies: str
     qase_x_xsfr_token: str
     project_name: str
     testit_token: str
-    testit_cookies: str = None
+    testit_cookies: str | None
     testit_projectId: str
     testit_sectionId: str
     url_testit: str
 
 
 class QaseData(BaseModel):
-    qase_number_test_cases: List[int]
+    qase_number_test_cases: list[int]
     qase_token: str
-    qase_url: str = "https://api.qase.io"
+    qase_url: str = Field(default="https://api.qase.io")
     project_name: str
     testit_projectId: str
     testit_sectionId: str
@@ -42,5 +36,5 @@ class QaseData(BaseModel):
 
 class TestItData(BaseModel):
     testit_token: str
-    testit_cookies: Optional[str] = None
+    testit_cookies: str | None
     url_testit: str
