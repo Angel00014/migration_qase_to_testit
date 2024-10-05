3bfc72b0ae884f076ae5dc01e9689ef716098544
Alex_work_note <alexchugunov4@gmail.com>
2024-10-05 13:22:16 +0300

    refactor schemas.py

--- src/model/schemas.py
+++ src/model/schemas.py
@@ -1,30 +1,6 @@
 from pydantic import BaseModel, Field
 
 
-class ReportDataV2(BaseModel):
-    qase_url: str | None
-    qase_number_test_cases: list[int]
-    qase_token: str
-    qase_project_name: str
-    testit_token: str
-    testit_cookies: str | None
-    testit_projectId: str
-    testit_sectionId: str
-    testit_url: str
-
-
-class ReportData(BaseModel):
-    number_test_cases: list[int]
-    qase_cookies: str
-    qase_x_xsfr_token: str
-    project_name: str
-    testit_token: str
-    testit_cookies: str | None
-    testit_projectId: str
-    testit_sectionId: str
-    url_testit: str
-
-
 class QaseData(BaseModel):
     qase_number_test_cases: list[int]
     qase_token: str
@@ -32,9 +8,3 @@ class QaseData(BaseModel):
     project_name: str
     testit_projectId: str
     testit_sectionId: str
-
-
-class TestItData(BaseModel):
-    testit_token: str
-    testit_cookies: str | None
-    url_testit: str
